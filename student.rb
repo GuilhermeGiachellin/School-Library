3f1f54fdb370d6d5d7759c6e7e33bcbf21ff26a6
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-10-13 18:10:40 -0300

    Added relations between objects

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require_relative 'person'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age, name, parent_permission, classroom)
     super(age, name, parent_permission)
     @classroom = classroom
@@ -8,5 +10,10 @@ class Student < Person
 
   def play_hook
     "¯\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.student.push(self) unless classroom.student.include?(self)
   end
 end
