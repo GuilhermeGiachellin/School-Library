4e8095029cbda266eb5b38d9ceae4cf9ed19a5d6
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-10-13 18:48:39 -0300

    Add starting vallue to classroom and parent_permission in Student class

--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, name, parent_permission, classroom)
-    super(age, name, parent_permission)
+  def initialize(age, name, parent_permission, classroom: nil)
+    super(age, name, parent_permission: true)
     @classroom = classroom
   end
 
@@ -14,6 +14,6 @@ class Student < Person
 
   def classroom=(classroom)
     @classroom = classroom
-    classroom.student.push(self) unless classroom.student.include?(self)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
